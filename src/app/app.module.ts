a1ed0a9f55ea3aeaef1c4ffbe16f8e88e403894d
amohapatra-infoblox <amohapatra@infoblox.com>
2025-05-10 16:05:26 +0530

    created new branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { OverviewComponent } from './home/overview/overview.component';
-import { DetailsComponent } from './home/details/details.component';
-import { TeamComponent } from './about/team/team.component';
-import { MissionComponent } from './about/mission/mission.component';
-import { InfoComponent } from './contact/info/info.component';
-import { FormComponent } from './contact/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { TableViewComponent } from './shared/table-view/table-view.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent,
-    OverviewComponent,
-    DetailsComponent,
-    TeamComponent,
-    MissionComponent,
-    InfoComponent,
-    FormComponent
+    ProductsComponent,
+    NavbarComponent,
+    TableViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    SharedModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
